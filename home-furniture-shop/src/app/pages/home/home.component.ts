7730fb599168086b6afac259d90d091942c0c8ef
codeUP <labesherwin@gmail.com>
2025-12-04 15:21:43 +0800

    process

--- home-furniture-shop/src/app/pages/home/home.component.ts
+++ home-furniture-shop/src/app/pages/home/home.component.ts
@@ -1,54 +1,55 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+interface Slide {
+  title: string;
+  category: string;
+  color: string; // background color for design
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],   // <-- REQUIRED for *ngFor
+  imports: [CommonModule], // <-- Required for *ngFor and *ngIf
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy {
-
-  slides = [
-    { image: 'assets/images/decor.jpg', title: 'Decor' },
-    { image: 'assets/images/essentials.jpg', title: 'Essentials' },
-    { image: 'assets/images/furniture.png', title: 'Furniture' },
+export class HomeComponent {
+  slides: Slide[] = [
+    { title: 'Modern Sofa', category: 'Furniture', color: '#FFB74D' },
+    { title: 'Elegant Vase', category: 'Decor', color: '#81C784' },
+    { title: 'Bed Sheets', category: 'Essentials', color: '#64B5F6' },
+    { title: 'Wooden Chair', category: 'Furniture', color: '#FF8A65' },
+    { title: 'Wall Art', category: 'Decor', color: '#4DB6AC' },
+    { title: 'Cooking Set', category: 'Essentials', color: '#7986CB' }
   ];
 
+  currentSlide: number = 0;
+  sliderInterval: any;
 
-
-  currentSlide = 0;
-  private intervalId: any;
-
-  ngOnInit(): void {
-    this.startAutoSlide();
+  constructor() {
+    this.startSlider();
   }
 
-  ngOnDestroy(): void {
-    this.pauseSlider();
-  }
-
-  startAutoSlide(): void {
-    this.intervalId = setInterval(() => {
-      this.nextSlide();
-    }, 3000);
+  startSlider(): void {
+    this.sliderInterval = setInterval(() => this.nextSlide(), 4000);
   }
 
   pauseSlider(): void {
-    clearInterval(this.intervalId);
+    clearInterval(this.sliderInterval);
   }
 
   resumeSlider(): void {
-    this.startAutoSlide();
-  }
-
-  nextSlide(): void {
-    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    this.startSlider();
   }
 
   prevSlide(): void {
     this.currentSlide =
-      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+      this.currentSlide === 0 ? this.slides.length - 1 : this.currentSlide - 1;
+  }
+
+  nextSlide(): void {
+    this.currentSlide =
+      this.currentSlide === this.slides.length - 1 ? 0 : this.currentSlide + 1;
   }
 }
